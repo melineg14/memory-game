65b27d3988b8f7562269547302a7767ef9bafca1
melineg14 <melinegeny14@gmail.com>
2020-02-04 15:27:18 +0100

    game finished

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
+var t2 = [];
+
 /**
  * Fonction principale, au chargement de la page web
  */
 const main = () => {
 }
 
-var t2 = [];
-
 /**
  * Fonction renvoyant le tableau des éléments du DOM à utiliser
  */
 const getBlocs = () => {
     return [
+        document.getElementById("color-bloc0"),
         document.getElementById("color-bloc1"),
         document.getElementById("color-bloc2"),
-        document.getElementById("color-bloc3"),
-        document.getElementById("color-bloc4")
+        document.getElementById("color-bloc3")
     ];
 }
 
@@ -60,6 +60,7 @@ const afficheBloc = (index) => {
  * Fonction lançant l'affichage aléatoire et renvoyant un t2[] contenant une combinaison aléatoire
  */
 const start = () => {
+    t2 = [];
     for (let i = 0; i < 4; i++) {
         setTimeout(() => {
             t2.push(afficheBloc(i));
@@ -73,18 +74,16 @@ const start = () => {
  * Fonction renvoyant un nouveau tableau (t3[]) contenant les clics de l'utilisateur
  */
 const getClicks = () => {
-    console.log("entree dans getclics")
-    const blocs = getBlocs()
+    const blocs = getBlocs();
     const t3 = [];
-    for (let i = 1; i < 5; i++) {
+    for (let i = 0; i < 4; i++) {
         document.getElementById("color-bloc" + i).addEventListener('click', (bloc) => {
-            console.log(bloc)
+            t3.push(i);
             if (blocs.length == t3.length) {
                 compare(t3);
             }
         }, false)
     }
-
     return t3;
 }
 
@@ -92,35 +91,24 @@ const getClicks = () => {
  * Fonction renvoyant un message selon si les tableaux sont égaux ou non
  */
 const compare = (t3) => {
-    let mess = docuement.getElementById("message");
-    if (t2 == t3) {
+    let mess = document.getElementById("message");
+    if (String(t2) == String(t3)) {
+        mess.classList.remove("hidden");
         mess.classList.add("winner");
         mess.textContent = "You won !";
+        setTimeout(() => {
+            mess.classList.add("hidden");
+            mess.classList.remove("winner");
+
+        }, 2000);
     }
     else {
+        mess.classList.remove("hidden");
         mess.classList.add("loser");
         mess.textContent = "Sorry you lost... Try again !";
+        setTimeout(() => {
+            mess.classList.add("hidden");
+            mess.classList.remove("loser");
+        }, 2000);
     }
 }
-
-
-
-
-
-
-
-// const getClicks = () => {
-//     const blocs = getBlocs();
-//     const t3 = [];
-//     for (bloc of blocs) {
-//         bloc.addEventListener('click', () => {
-//             indexBloc = blocs.indexOf(bloc);
-//             t3.push(indexBloc);
-//         }, false)
-//     }
-//     console.log("t3 :", t3);
-//     if (blocs.length == t3.length) {
-//         compare();
-//     }
-//     return t3;
-// }
